15141afc119213198cd4dd7d2a04d35879e3b010
Fernando Fogliato <fernandofogliato@gmail.com>
2019-12-17 23:19:54 -0300

    Style view students

--- frontend/src/pages/Student/index.js
+++ frontend/src/pages/Student/index.js
@@ -1,38 +1,57 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { MdAdd, MdModeEdit, MdDelete } from 'react-icons/md';
+
+import { Container, Title, StudentTable } from './styles';
 
 export default function Student() {
   return (
     <Container>
-      <h2>Gerenciamento alunos</h2>
-      <table>
+      <Title>
+        <h2>Gerenciando alunos</h2>
+        <div>
+          <button type="button" title="Cadastrar novo aluno">
+            <MdAdd color="#fff" size={20} />
+            NOVO
+          </button>
+          <input type="text" placeholder="Buscar aluno" />
+        </div>
+      </Title>
+      <StudentTable>
         <thead>
           <th>NOME</th>
           <th>E-MAIL</th>
-          <th>IDADE</th>
+          <th style={{ textAlign: 'left' }}>IDADE</th>
         </thead>
         <tbody>
           <tr>
             <td>Cha Ji-Hum</td>
             <td>example@rocketseat.com.br</td>
-            <td>20</td>
+            <td align="center">20</td>
             <td>
-              <button type="button">editar</button>
-              <button type="button">apagar</button>
+              <button type="button" title="Editar aluno">
+                <MdModeEdit color="#ee4d64" size={20} />
+              </button>
+              <button type="button" title="Apagar aluno">
+                <MdDelete color="#ee4d64" size={20} />
+              </button>
             </td>
           </tr>
           <tr>
             <td>Cha Ji-Hum</td>
             <td>example@rocketseat.com.br</td>
-            <td>20</td>
+            <td align="center">20</td>
             <td>
-              <button type="button">editar</button>
-              <button type="button">apagar</button>
+              <button type="button" title="Editar">
+                <MdModeEdit color="#ee4d64" size={20} />
+              </button>
+              <button type="button" title="Apagar">
+                <MdDelete color="#ee4d64" size={20} />
+              </button>
             </td>
           </tr>
         </tbody>
-      </table>
+      </StudentTable>
     </Container>
   );
 }
--- frontend/src/pages/Student/styles.js
+++ frontend/src/pages/Student/styles.js
@@ -1,3 +1,88 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 35px auto;
+  color: #444;
+`;
+
+export const Title = styled.div`
+  margin: 10px 0 25px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-size: 16px;
+
+  div {
+    display: flex;
+  }
+
+  button {
+    background-color: #ee4d64;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+  }
+
+  input {
+    padding: 5px;
+    border-radius: 4px;
+    border: 1px solid #ddd;
+    color: #666;
+  }
+`;
+
+export const StudentTable = styled.table`
+  width: 100%;
+  background: #fff;
+  border-radius: 4px;
+
+  thead th {
+    text-align: left;
+    padding: 12px;
+  }
+
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+  }
+
+  img {
+    height: 100px;
+  }
+
+  strong {
+    color: #333;
+    display: block;
+  }
+
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`;
